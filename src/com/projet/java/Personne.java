bf06599aa7739ae30a263a6754f7ef3176fd0b18
Yao Josué <jyao2000@yahoo.fr>
2020-08-30 16:16:40 +0000

    function to create a new Personne from scratch

--- src/com/projet/java/Personne.java
+++ src/com/projet/java/Personne.java
@@ -3,6 +3,7 @@ package com.projet.java;
 
 import java.io.Serializable;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Personne implements Serializable {
     String nom;
@@ -58,6 +59,40 @@ public class Personne implements Serializable {
         this.parent = parent;
         this.freres = new LinkedList<>();
         this.enfants = new LinkedList<>();
+    }
+    public Personne(String option){
+        if (option=="New"){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Entrer le nom: ");
+            this.nom = sc.nextLine();
+            System.out.print("Entrer le prenom: ");
+            this.prenoms = sc.nextLine();
+            while(true)
+            {
+                Process.affichage("\nSexe:\n 1. Masculin\n 2. Feminin\nChoix: ");
+
+                int opt=Integer.parseInt(sc.next());
+                if(opt==1)
+                {
+                    this.sexe = "Masculin";
+                    break;
+                }
+                else if(opt==2)
+                {
+                    this.sexe = "Féminin";
+                    break;
+                }
+                else
+                {
+                    Process.affichage("\nMauvais choix\nVeuiller recommencer...  ");
+                }
+            }
+        }
+        this.statut = "Cadet";
+        this.parent = null;
+        this.freres_personne = new LinkedList<>();
+        this.enfants = new LinkedList<>();
+
     }
 
     public void addEnfant(Personne enfant){
