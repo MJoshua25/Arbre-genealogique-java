0ca9b38ea8647eaac0106f077824db7b551d4312
Yao Josué <jyao2000@yahoo.fr>
2020-08-30 12:30:41 +0000

    get properties

--- src/com/projet/java/Personne.java
+++ src/com/projet/java/Personne.java
@@ -6,5 +6,12 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Personne implements Serializable {
+    String nom;
+    String prenoms;
+    String sexe;
+    String statut;
+    Personne parent;
+    LinkedList<Personne> enfants;
+    LinkedList<Personne> freres;
 
 }
