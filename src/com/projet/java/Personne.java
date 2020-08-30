d7f51ee0eeafca17ec696debfa2e435fe2694c83
Yao Josué <jyao2000@yahoo.fr>
2020-08-30 12:27:21 +0000

    import and implement

--- src/com/projet/java/Personne.java
+++ src/com/projet/java/Personne.java
@@ -1,5 +1,10 @@
 package com.projet.java;
 
-public class Personne {
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Personne implements Serializable {
 
 }
